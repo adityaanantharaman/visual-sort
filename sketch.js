34d31f5fda6fa3593af74383d4106bc64229d4ed
adityaanantharaman <ananthram.aditya@gmail.com>
2020-06-13 20:21:32 +0530

    adding merge sort

--- sketch.js
+++ sketch.js
@@ -18,6 +18,15 @@ var whatqsdo=0;
 var fast,slow;
 var sorted_by_qs=[];
 
+var ms=0;
+var lptr,rptr,lend,rend;
+var pma=[];
+var sopma,wipma;
+var tosort=[];
+var sorted=[];
+var tomerge=[];
+var whatmsdo,startingmerge,phase2;
+
 var downing,mainheapify,hsmax,whathsdo;
 
 function setup_init(s)
@@ -54,6 +63,7 @@ function setup() {
     sel.option("BUBBLE SORT");
     sel.option("QUICK SORT");
     sel.option("HEAP SORT");
+    sel.option("MERGE SORT");
     sel.changed(changeSort);
 }
 
@@ -72,6 +82,7 @@ function reset()
     bs=0;
     qs=0;
     hs=0;
+    ms=0;
     arr=[];
     sorted_by_qs=[];
     for(let i=0;i<size;i++)
@@ -87,6 +98,8 @@ function changeSort()
         curr_sort=2;
     else if(sel.value()=="HEAP SORT")
         curr_sort=3;
+    else if(sel.value()=="MERGE SORT")
+        curr_sort=4;
     reset();
 }
 
@@ -434,6 +447,159 @@ function hs_down_step(h,newsize)
                 }
         }
 }
+function ms_init()
+{
+    tosort=[];
+    sorted=[];
+    tomerge=[];
+    tosort.push([0,size-1]);
+    whatmsdo=0;
+    startingmerge=0;
+    lptr=-1;
+    rptr=-1;
+    lend=-1;
+    rend=-1;
+    pma=[];//partly merged array
+    sopma=-1;//expected size of merged array
+    wipma=-1;//where in partly merged array
+    phase2=0;
+}
+function ms_show()
+{
+    stroke(255,0,0);
+    strokeWeight(1);
+    fill(100,50,255);
+    for(let i=0;i<size;i++)
+        {
+            if(whatmsdo==1)
+                {
+                    if(i<=rend && i>=rend-sopma+1)
+                        {
+                            stroke(color('#d92027'));
+                            fill(color('#d92027')); 
+                        }
+                    else
+                        {
+                            stroke(color('#035aa6'));
+                            fill(color('#035aa6')); 
+                        }
+                }
+            else if(whatmsdo==2)
+                {
+                    stroke(color('#d92027'));
+                    fill(color('#d92027')); 
+                }
+            else
+                {
+                    stroke(color('#035aa6'));
+                    fill(color('#035aa6'));   
+                }
+            rect(15+i*(bar_width+gap),height-arr[i],bar_width,arr[i],10);   
+        }
+    if(whatmsdo==1)
+        {
+            for(let i=0;i<pma.length;i++)
+                {
+                    stroke(color('#fcbf1e'));
+                    fill(color('#fcbf1e'));
+                    let tempi=i+rend-sopma+1;
+                    rect(15+tempi*(bar_width+gap),height-pma[i],bar_width/2,pma[i],10);  
+                }
+        }
+            
+    
+}
+function ms_step()
+{
+    if(whatmsdo==0)
+        {
+            if(tomerge.length>0)
+            {
+                if(sorted.length>1)
+                    {
+                        let l=tomerge[tomerge.length-1][0];
+                        let r=tomerge[tomerge.length-1][2];
+                        let mid=tomerge[tomerge.length-1][1];
+                        if(sorted[sorted.length-1][0]==mid && sorted[sorted.length-1][1]==r && sorted[sorted.length-2][0]==l && sorted[sorted.length-2][1]==mid-1)
+                            {
+                                sorted.pop();
+                                sorted.pop();
+                                whatmsdo=1;
+                                startingmerge=1;
+                            }
+                    }
+            }
+            if(whatmsdo==0 && tosort.length>0)
+                {
+                    let curset=tosort.pop();
+                    let l=curset[0];
+                    let r=curset[1];
+                    if(l!=r)
+                        {
+                            let mid=int((l+r)/2);
+                            tosort.push([mid+1,r]);
+                            tosort.push([l,mid]);
+                            tomerge.push([l,mid+1,r]);
+                        }
+                    else
+                        sorted.push([l,l]);
+                }
+        }
+    else if(whatmsdo==1)
+        {
+            if(startingmerge==1)
+                {
+                    let merblo=tomerge.pop();
+                    lptr=merblo[0];
+                    rptr=merblo[1];
+                    lend=merblo[1]-1;
+                    rend=merblo[2];
+                    startingmerge=0;
+                    sopma=rend-lptr+1;
+                }
+            else
+                {
+                    if(pma.length==sopma)
+                        {
+                            for(let i=0;i<sopma;i++)
+                                arr[i+rend-sopma+1]=pma[i];
+                            pma=[];
+                            startingmerge=1;
+                            whatmsdo=0;
+                            phase2=0;
+                            sorted.push([rend-sopma+1,rend]);
+                            wipma=-1;
+                            if(sopma==size)
+                                whatmsdo=2;
+                        }
+                    else
+                        {
+                            if(phase2==0 && (lptr>lend || rptr>rend))
+                               {
+                                    if(lptr>lend)
+                                        {
+                                            lptr=rptr;
+                                            lend=rend;
+                                        }
+                                   phase2=1;
+                               }
+                            if(phase2==0)
+                                {
+                                    if(arr[lptr]<arr[rptr])
+                                        pma[++wipma]=arr[lptr++];
+                                    else
+                                        pma[++wipma]=arr[rptr++];
+                                }
+                            else if(phase2==1)
+                                {
+                                    pma[++wipma]=arr[lptr++];
+                                }
+                        }
+                    
+                }
+        }
+
+}
 
 function draw() {
   // put drawing code here
@@ -486,6 +652,18 @@ function draw() {
                         }
                     hs_show();
                     hs_step();
+                    break;
+                }
+            case(4):
+                {
+                    if(ms==0)
+                        {
+                            ms=1;
+                            ms_init();
+                        }
+                    ms_show();
+                    ms_step();
+                    break;
                 }
                 
         }
